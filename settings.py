c40da9bab4610f8c23e067eb61be8a8d356e84dc
Vedha <vraje059@uottawa.ca>
2016-03-10 20:45:19 -0500

    Taking away previous changes

--- settings.py
+++ settings.py
@@ -126,36 +126,12 @@ works = {
             },
         },
     }
-momenta = {
-    'schema': {
-        'title': {
-            'type': 'string',
-            'required': True,
-        },
-        'description': {
-            'type': 'string',
-        },
-        'owner': {
-            'type': 'objectid',
-            'required': True,
-            # referential integrity constraint: value must exist in the
-            # 'people' collection. Since we aren't declaring a 'field' key,
-            # will default to `people._id` (or, more precisely, to whatever
-            # ID_FIELD value is).
-            'data_relation': {
-                'resource': 'people',
-                # make the owner embeddable with ?embedded={"owner":1}
-                'embeddable': True
-            },
-        },
     }
 
-}
 
 # The DOMAIN dict explains which resources will be available and how they will
 # be accessible to the API consumer.
 DOMAIN = {
     'people': people,
     'works': works,
-    'momenta': momenta,
 }
